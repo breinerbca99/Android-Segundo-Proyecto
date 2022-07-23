bb8cc7a5e333ecb4ed2f904f7f2215fdf9319280
breiner.correa.contractor <breinecito@hotmail.com>
2022-07-23 15:48:34 -0500

    añadiendo configuracion para permitir fragments

---
+++ app/src/main/java/com/example/segundoproyecto/PrimerFragment.kt
@@ -0,0 +1,17 @@
+package com.example.segundoproyecto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+/* TODO Este fragment, extienden de Fragment
+    y recibe como parametro un LayoutRes */
+class PrimerFragment : Fragment(R.layout.fragment_primer) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
